37b74f86d1644b47379f625664a810129d5c6431
Lue <escasinasluezyldominic@gmail.com>
2026-02-18 16:46:44 +0800

    feat(be): - created a JwtProvider - configured spring security settings     - removed default authentication and authorization settings

---
+++ backend/userAuth/src/main/java/com/escasinas/userAuth/security/JwtProvider.java
@@ -0,0 +1,63 @@
+package com.escasinas.userAuth.security;
+
+import com.escasinas.userAuth.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtProvider {
+
+    @Value("${app.jwt.secret}")
+    public String secretKey;
+
+
+    public String generateToken(User user) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .setSubject(String.valueOf(user.username))
+                .claim("userId", user.userId)
+                .claim("email", user.emailAddress)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        try {
+
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            System.out.println("Invalid JWT: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public String extractUsername(String token) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
